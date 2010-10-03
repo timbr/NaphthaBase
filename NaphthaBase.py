f47fe30c472b1f0601d1e9410aa3e9178e46d9a3
Tim <tibrowning@gmail.com>
2010-10-03 21:33:59 +0100

    Tidied up as per PEP 8 Style Guide.

--- NaphthaBase.py
+++ NaphthaBase.py
@@ -1,25 +1,43 @@
-from Settings import *
+""" naphthabase.py  -- Interface to the NaphthaBase Database.
+
+Provides routines for taking a snapshot of the official R & R Associates 
+database and saving the data in the NaphthaBase Database. All changes since
+the last snapshot are added without deleting the older data.
+
+Routines for quick and easy access to data in the NaphthaBase are also
+provided.
+"""
+
 import pyodbc
 import os
 import sqlite3
 import datetime
 
+from Settings import *
+
+
 NaphthaBaseChecked = 0
 stock_connection = ''
 
 
-def MakeDatabaseConnection(RandR_db = RandR_Naphtha_Dbase):
+def make_database_connection(RandR_db = RandR_Naphtha_Dbase):
+    """Make a connection to the Official R&R database or any other db given"""
+    
     global stock_connection
-    CheckTables() # check that all the necessary tables are present
+    check_tables() # check that all the necessary tables are present
     
     if os.path.exists(RandR_db):
         stock_connection = \
-        pyodbc.connect(DRIVER='{Microsoft Access Driver (*.mdb)}', DBQ=RandR_db)
+           pyodbc.connect(DRIVER='{Microsoft Access Driver (*.mdb)}',
+                       DBQ=RandR_db)
         print "Made connection with R&R database at %s" % RandR_db
     else:
         print "Unable to make connection with R&R database at %s" % RandR_db
 
-def CheckTables():
+        
+def check_tables():
+    """Checks that the NaphthaBase has all the tables it needs and no more."""
+    
     global NaphthaBaseChecked # TODO Find alternative to global variables
     NaphthaBase = sqlite3.connect(NaphthaBase_Dbase)
     c = NaphthaBase.cursor()
@@ -28,8 +46,14 @@ def CheckTables():
     print tables
     if len(tables) == 0:
         # empty database file
-        c.execute("create table Material (Code text, Description text, LastUpdated date, RecordNo int)")
-        c.execute("create table Purchases (PO_Num text, Code text, Batch text, Quantity text, Price text, OrderValue text, Supplier text, OrderReference text, OrderDate date, DueDate date, PlacedBy text, DeliveredQuantity text, PrintedComment text, DeliveryComment text, Status int, LastUpdated date)")
+        c.execute("create table Material (Code text, Description text, \
+                  LastUpdated date, RecordNo int)")
+        c.execute("create table Purchases (PO_Num text, Code text, \
+                  Batch text, Quantity text, Price text, OrderValue text, \
+                  Supplier text, OrderReference text, OrderDate date, \
+                  DueDate date, PlacedBy text, DeliveredQuantity text, \
+                  PrintedComment text, DeliveryComment text, Status int, \
+                  LastUpdated date)")
         NaphthaBase.commit()
     if 'Material_temp' in tables:
         c.execute("drop table Material_temp")
@@ -42,42 +66,47 @@ def CheckTables():
     NaphthaBase.close()
     NaphthaBaseChecked = 1
         
-class materialcodes(object):
+        
+class MaterialCodes(object):
+    """Updates and provides access to Material Codes and their descriptions"""
+    
     def __init__(self):
         if NaphthaBaseChecked == 0:
             # No connection has been made to the NaphthaBase
             MakeDatabaseConnection()
         if stock_connection == '':
             # No connection has been made to the R&R stock database
-            self.localonly = 1
+            self._localonly = 1
         else:
-            self.localonly = 0
+            self._localonly = 0
         NaphthaBase = sqlite3.connect(NaphthaBase_Dbase)
         c = NaphthaBase.cursor()
-        self.matdata = [row for row in c.execute("select * from Material")]
-        self.createDB()
+        self._matdata = [row for row in c.execute("select * from Material")]
+        self._create_db()
     
     def getdesc(self, matcode):
         return self.getlatest.get(matcode.upper().upper(), [''])[0]
         # Return an empty string if no key found
     
-    def createDB(self):
+    def _create_db(self):
         # Constants to make list items clearer
         CODE = 0
         DESC = 1
         LASTUPDATE = 2
         RECORDNUM = 3
         self.getlatest = {}
-        for entry in self.matdata:
+        for entry in self._matdata:
             if entry[CODE] in self.getlatest.keys():
                 if entry[LASTUPDATE] > self.getlatest[entry[CODE]][LASTUPDATE]:
-                    self.getlatest[entry[CODE]] = [entry[DESC], entry[RECORDNUM], entry[LASTUPDATE]]
+                    self.getlatest[entry[CODE]] = [entry[DESC], \
+                       entry[RECORDNUM], entry[LASTUPDATE]]
                     # RECORDNUM is added in position 1 so that LASTUPDATE remains in position 2
             else:
-                self.getlatest[entry[CODE]] = [entry[DESC], entry[RECORDNUM], entry[LASTUPDATE]]
+                self.getlatest[entry[CODE]] = [entry[DESC], entry[RECORDNUM],\
+                   entry[LASTUPDATE]]
         
-    def updateNaphthaBase(self):
-        if self.localonly == 1:
+    def update_naphtha_base(self):
+        if self._localonly == 1:
             print 'Unable to update - remote database not found'
             return
         RandRcursor = stock_connection.cursor()
@@ -95,7 +124,8 @@ class materialcodes(object):
         
         NaphthaBase = sqlite3.connect(NaphthaBase_Dbase)
         c = NaphthaBase.cursor()
-        c.execute("create table Material_temp (Code text, Description text, LastUpdated date, RecordNo int)")
+        c.execute("create table Material_temp (Code text, Description text, \
+                  LastUpdated date, RecordNo int)")
         for entry in RandRdata:
             c.execute('insert into Material_temp values (?,?,?,?)', entry)
         NaphthaBase.commit()
@@ -103,55 +133,65 @@ class materialcodes(object):
         diffquery = """
         SELECT Material_temp.*
         from Material_temp
-        Left Join Material on (Material_temp.LastUpdated = Material.LastUpdated)
+        Left Join Material on
+        (Material_temp.LastUpdated = Material.LastUpdated)
         where Material.LastUpdated IS Null
-        """  
+        """
+        
         diff = c.execute(diffquery)
-        
         newrows =  [row for row in diff]
-        
         for entry in newrows:
             c.execute('insert into Material values (?,?,?,?)', entry)
         NaphthaBase.commit()
-        self.matdata = [row for row in c.execute("select * from Material")]
-        self.createDB()
+        self._matdata = [row for row in c.execute("select * from Material")]
+        self._create_db()
         NaphthaBase.close()
 
-class purchases(object):
+        
+class Purchases(object):
+    """Updates and provides access to Purchase Order information."""
+    
     def __init__(self):
         if NaphthaBaseChecked == 0:
             # No connection has been made to the NaphthaBase
             MakeDatabaseConnection()
         if stock_connection == '':
             # No connection has been made to the R&R stock database
-            self.localonly = 1
+            self._localonly = 1
         else:
-            self.localonly = 0
+            self._localonly = 0
         NaphthaBase = sqlite3.connect(NaphthaBase_Dbase)
         NaphthaBase.text_factory = str # solves problem with Pound signs....!
         c = NaphthaBase.cursor()
-        self.POdata = [row for row in c.execute("select * from Purchases")]
-        #self.createDB()
+        self._POdata = [row for row in c.execute("select * from Purchases")]
+        #self._create_db()
     
-    def getPO(self, PO_Num):
+    def get_po(self, PO_Num):
         NaphthaBase = sqlite3.connect(NaphthaBase_Dbase)
         NaphthaBase.text_factory = str # solves problem with Pound signs....!
         c = NaphthaBase.cursor()
         command = """
-        SELECT PO_Num, Code, Batch, Quantity, Price, OrderValue, Supplier, OrderReference, OrderDate,
-        DueDate, PlacedBy, DeliveredQuantity, PrintedComment, DeliveryComment, Status, LastUpdated
+        SELECT PO_Num, Code, Batch, Quantity, Price, OrderValue, Supplier,
+        OrderReference, OrderDate, DueDate, PlacedBy, DeliveredQuantity,
+        PrintedComment, DeliveryComment, Status, LastUpdated
         FROM Purchases,
-        (SELECT MAX(LastUpdated) AS latest from Purchases WHERE PO_Num = %(po_num)s)
+        (SELECT MAX(LastUpdated) AS latest from Purchases WHERE
+        PO_Num = %(po_num)s)
         WHERE PO_Num = %(po_num)s and LastUpdated = latest
         ORDER BY Code
         """ % {'po_num': str(PO_Num)}
         results = c.execute(command)
         return [line for line in results]
     
-    def createDB(self):
+    def _create_db(self):
         PO_conn = sqlite3.connect(':memory:')
         PO_c = PO_conn.cursor()
-        PO_c.execute("create table Purchases (PO_Num text, Code text, Batch text, Quantity text, Price text, OrderValue text, Supplier text, OrderReference text, OrderDate date, DueDate date, PlacedBy text, DeliveredQuantity text, PrintedComment text, DeliveryComment text, Status int, LastUpdated date)")
+        PO_c.execute("create table Purchases (PO_Num text, Code text, \
+                     Batch text, Quantity text, Price text, OrderValue text, \
+                     Supplier text, OrderReference text, OrderDate date, \
+                     DueDate date, PlacedBy text, DeliveredQuantity text, \
+                     PrintedComment text, DeliveryComment text, Status int, \
+                     LastUpdated date)")
         PO_c.commit()
         
         NaphthaBase = sqlite3.connect(NaphthaBase_Dbase)
@@ -168,13 +208,15 @@ class purchases(object):
         for entry in self.POdata:
             if entry[CODE] in self.getlatest.keys():
                 if entry[LASTUPDATE] > self.getlatest[entry[CODE]][LASTUPDATE]:
-                    self.getlatest[entry[CODE]] = [entry[DESC], entry[RECORDNUM], entry[LASTUPDATE]]
+                    self.getlatest[entry[CODE]] = [entry[DESC], \
+                       entry[RECORDNUM], entry[LASTUPDATE]]
                     # RECORDNUM is added in position 1 so that LASTUPDATE remains in position 2
             else:
-                self.getlatest[entry[CODE]] = [entry[DESC], entry[RECORDNUM], entry[LASTUPDATE]]
+                self.getlatest[entry[CODE]] = [entry[DESC], entry[RECORDNUM],\
+                   entry[LASTUPDATE]]
         
-    def updateNaphthaBase(self):
-        if self.localonly == 1:
+    def update_naphtha_base(self):
+        if self._localonly == 1:
             print 'Unable to update - remote database not found'
             return
         RandRcursor = stock_connection.cursor()
@@ -197,7 +239,8 @@ class purchases(object):
         \"Purchase Order\".Status,
         \"Purchase Item\".\"Last Updated\" AS LastUpdated
         FROM \"Purchase Item\", \"Purchase Order\", \"Formula Stock\"
-        WHERE (\"Purchase Item\".\"Order Number\" = \"Purchase Order\".\"Order Number\"
+        WHERE (\"Purchase Item\".\"Order Number\" = 
+        \"Purchase Order\".\"Order Number\"
         AND \"Purchase Item\".\"Order Number\" = \"Formula Stock\".PON
         AND \"Purchase Item\".\"Component Code\" = \"Formula Stock\".Key)
         ORDER BY \"Purchase Order\".\"Order Number\"
@@ -207,52 +250,73 @@ class purchases(object):
         NaphthaBase = sqlite3.connect(NaphthaBase_Dbase)
         NaphthaBase.text_factory = str # solves problem with Pound signs....!
         c = NaphthaBase.cursor()
-        c.execute("create table Purchases_temp (PO_Num text, Code text, Batch text, Quantity text, Price text, OrderValue text, Supplier text, OrderReference text, OrderDate date, DueDate date, PlacedBy text, DeliveredQuantity text, PrintedComment text, DeliveryComment text, Status int, LastUpdated date)")
+        c.execute("create table Purchases_temp (PO_Num text, Code text, \
+                  Batch text, Quantity text, Price text, OrderValue text, \
+                  Supplier text, OrderReference text, OrderDate date, \
+                  DueDate date, PlacedBy text, DeliveredQuantity text, \
+                  PrintedComment text, DeliveryComment text, Status int, \
+                  LastUpdated date)")
         for entry in RandRdata:
-            entry_temp = [thing for thing in entry]
-            entry_temp[3] = str(entry_temp[3]) # prices and quantities are stored as a decimal type in the RandR database
-            entry_temp[4] = str(entry_temp[4]) # Convert them to text fields for storing in the NaphthaBase
-            entry_temp[5] = str(entry_temp[5])
+            entry_temp = [thing for thing in entry] #prices and quantities are
+            entry_temp[3] = str(entry_temp[3]) #stored as a decimal type in the
+            entry_temp[4] = str(entry_temp[4]) #RandR database. Convert them to text
+            entry_temp[5] = str(entry_temp[5]) #fields for storing in the NaphthaBase
             entry_temp[11] = str(entry_temp[11])
-            c.execute('insert into Purchases_temp values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', entry_temp)
+            c.execute('insert into Purchases_temp values (?,?,?,?,?,?,?,?,?, \
+                      ?,?,?,?,?,?,?)', entry_temp)
         NaphthaBase.commit()
         
         diffquery = """
         SELECT Purchases_temp.*
         from Purchases_temp
-        Left Join Purchases on (Purchases_temp.LastUpdated = Purchases.LastUpdated)
+        Left Join Purchases on (Purchases_temp.LastUpdated =
+        Purchases.LastUpdated)
         where Purchases.LastUpdated IS Null
-        """  
+        """
+        
         diff = c.execute(diffquery)
-        
         newrows =  [row for row in diff]
-        
-        for entry in newrows:
-            
-            c.execute('insert into Purchases values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)', entry)
+        for entry in newrows: 
+            c.execute('insert into Purchases values (?,?,?,?,?,?,?,?,?,?,?, \
+                      ?,?,?,?,?)', entry)
         NaphthaBase.commit()
         self.POdata = [row for row in c.execute("select * from Purchases")]
-        #self.createDB()
+        #self._create_db()
         NaphthaBase.close()        
         
-
 
 if __name__ == '__main__':
     # Tests
     if os.path.exists(NaphthaBase_Dbase):
         os.remove(NaphthaBase_Dbase) # delete existing NaphthaBase file
-    MakeDatabaseConnection(TestDB_Old)
-    MatCodeOld = materialcodes()
-    MatCodeOld.updateNaphthaBase()
+    make_database_connection(TestDB_Old)
+    MatCodeOld = MaterialCodes()
+    MatCodeOld.update_naphtha_base()
     original = MatCodeOld.getdesc('pa23')
     no_entry = MatCodeOld.getdesc('ppld26c')
     
-    MakeDatabaseConnection(TestDB_New)
-    MatCodeNew = materialcodes()
-    MatCodeNew.updateNaphthaBase()
+    po = Purchases()
+    po.update_naphtha_base()
+    podata_original = po.get_po(7562)
+    
+    make_database_connection(TestDB_New)
+    MatCodeNew = MaterialCodes()
+    MatCodeNew.update_naphtha_base()
     revised = MatCodeNew.getdesc('PA23')
     an_entry = MatCodeNew.getdesc('PPLD26C')
+    
+    po = Purchases()
+    po.update_naphtha_base()
+    podata_revised = po.get_po(7562)
     
     print
     print original, revised
     print no_entry, an_entry
+    print
+    print podata_original
+    print
+    print podata_revised
+    
+    
+    
+    
