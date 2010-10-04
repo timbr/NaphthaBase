54d1d90511ddf416011e7e35a2bc181e6f518c15
Tim <tibrowning@gmail.com>
2010-10-04 23:03:14 +0100

    First attempt at getting stock table. Needs more work on returning the all entries for a particular batch that are also all the latest entry versions.

--- NaphthaBase.py
+++ NaphthaBase.py
@@ -54,6 +54,14 @@ def check_tables():
                   DueDate date, PlacedBy text, DeliveredQuantity text, \
                   PrintedComment text, DeliveryComment text, Status int, \
                   LastUpdated date)")
+        c.execute("create table Stock (Batch text, Code text, Revision text, \
+                  BatchStatus text, QuantityNow text, \
+                  OriginalDeliveredQuantity text, StockInfo text, \
+                  Supplier text, PONumber text, PurchaseCost text, \
+                  Customer text, WONumber text, Price text, \
+                  UsageReference text, StockAction text, ItemOrder text, \
+                  QuantityMovement text, UserID text, LastUpdated date, \
+                  InvoiceDate date, BatchUp_Date date)")
         NaphthaBase.commit()
     if 'Material_temp' in tables:
         c.execute("drop table Material_temp")
@@ -63,6 +71,10 @@ def check_tables():
         c.execute("drop table Purchases_temp")
         NaphthaBase.commit()
         print 'Removed Purchases_temp table'
+    if 'Stock_temp' in tables:
+        c.execute("drop table Stock_temp")
+        NaphthaBase.commit()
+        print 'Removed Stock_temp table'
     NaphthaBase.close()
     NaphthaBaseChecked = 1
         
@@ -297,7 +309,157 @@ class Purchases(object):
         
         self.POdata = [row for row in c.execute("select * from Purchases")]
         #self._create_db()
-        NaphthaBase.close()        
+        NaphthaBase.close()
+        
+        
+class Stock(object):
+    """Updates and provides access to Stock information."""
+    
+    def __init__(self):
+        if NaphthaBaseChecked == 0:
+            # No connection has been made to the NaphthaBase
+            MakeDatabaseConnection()
+        if stock_connection == '':
+            # No connection has been made to the R&R stock database
+            self._localonly = 1
+        else:
+            self._localonly = 0
+        NaphthaBase = sqlite3.connect(NaphthaBase_Dbase)
+        NaphthaBase.text_factory = str # solves problem with Pound signs....!
+        c = NaphthaBase.cursor()
+        self._Stockdata = [row for row in c.execute("select * from Stock")]
+        #self._create_db()
+    
+    def get_batch(self, Batch_Num):
+        NaphthaBase = sqlite3.connect(NaphthaBase_Dbase)
+        NaphthaBase.text_factory = str # solves problem with Pound signs....!
+        c = NaphthaBase.cursor()
+        command = """
+        SELECT Batch, Code, Revision, BatchStatus, QuantityNow, \
+        OriginalDeliveredQuantity, StockInfo, Supplier, PONumber, \
+        PurchaseCost, Customer, WONumber, Price, UsageReference, \
+        StockAction, ItemOrder, QuantityMovement, UserID, LastUpdated, \
+        InvoiceDate, BatchUp_Date
+        FROM Stock,
+        (SELECT MAX(LastUpdated) AS latest from Stock WHERE
+        Batch = %(batch_num)s)
+        WHERE Batch = %(batch_num)s and LastUpdated = latest
+        """ % {'batch_num': str(Batch_Num)}
+        results = c.execute(command)
+        return [line for line in results]
+    
+    def _create_db(self):
+        Stock_conn = sqlite3.connect(':memory:')
+        Stock_c = PO_conn.cursor()
+        Stock_c.execute("create table Purchases (PO_Num text, Code text, \
+                     Batch text, Quantity text, Price text, OrderValue text, \
+                     Supplier text, OrderReference text, OrderDate date, \
+                     DueDate date, PlacedBy text, DeliveredQuantity text, \
+                     PrintedComment text, DeliveryComment text, Status int, \
+                     LastUpdated date)")
+        Stock_c.commit()
+        
+        NaphthaBase = sqlite3.connect(NaphthaBase_Dbase)
+        NaphthaBase.text_factory = str # solves problem with Pound signs....!
+        Stock_c_disk = NaphthaBase.cursor()
+
+        
+        # Constants to make list items clearer
+        CODE = 0
+        DESC = 1
+        LASTUPDATE = 2
+        RECORDNUM = 3
+        self.getlatest = {}
+        for entry in self.POdata:
+            if entry[CODE] in self.getlatest.keys():
+                if entry[LASTUPDATE] > self.getlatest[entry[CODE]][LASTUPDATE]:
+                    self.getlatest[entry[CODE]] = [entry[DESC], \
+                       entry[RECORDNUM], entry[LASTUPDATE]]
+                    # RECORDNUM is added in position 1 so that LASTUPDATE remains in position 2
+            else:
+                self.getlatest[entry[CODE]] = [entry[DESC], entry[RECORDNUM],\
+                   entry[LASTUPDATE]]
+        
+    def update_naphtha_base(self):
+        if self._localonly == 1:
+            print 'Unable to update - remote database not found'
+            return
+        RandRcursor = stock_connection.cursor()
+        command = """
+        SELECT \"Formula Stock\".Batch,
+        \"Formula Stock Usage\".Formula AS Code,
+        \"Formula Stock Usage\".Revision,
+        \"Formula Stock\".Type AS BatchStatus,
+        \"Formula Stock\".Quantity AS QuantityNow,
+        \"Formula Stock\".\"Original Quantity\" AS OriginalDeliveredQuantity,
+        \"Formula Stock\".Location AS StockInfo,
+        \"Formula Stock\".Supplier,
+        \"Formula Stock\".PON AS PONumber,
+        \"Formula Stock\".Cost AS PurchaseCost,
+        \"Formula Stock Usage\".Customer,
+        \"Formula Stock Usage\".\"Works order Number\" AS WONumber,
+        \"Formula Stock Usage\".Price,
+        \"Formula Stock Usage\".\"Usage Reference\" AS UsageReference,
+        \"Formula Stock Usage\".\"Record Type\" AS StockAction,
+        \"Formula Stock Usage\".\"Item Order\" AS ItemOrder,
+        \"Formula Stock Usage\".Quantity AS QuantityMovement,
+        \"Formula Stock Usage\".\"User ID\" AS UserID,
+        \"Formula Stock Usage\".\"Last Updated\" AS LastUpdated,
+        \"Formula Stock\".\"Last Updated\" AS InvoiceDate,
+        \"Formula Stock\".\"Production Date\" AS BatchUp_Date
+        FROM \"Formula Stock\", \"Formula Stock Usage\"
+        WHERE \"Formula Stock\".Batch = \"Formula Stock Usage\".Batch
+        ORDER BY \"Formula Stock\".Batch,
+        \"Formula Stock Usage\".\"Last Updated\"
+        """
+        RandRdata = RandRcursor.execute(command)
+        
+        NaphthaBase = sqlite3.connect(NaphthaBase_Dbase)
+        NaphthaBase.text_factory = str # solves problem with Pound signs....!
+        c = NaphthaBase.cursor()
+        
+        query = c.execute("select * from sqlite_master where type = 'table'")
+        tables = [row[1] for row in query]
+        if 'Stock_temp' in tables:
+            c.execute("drop table Stock_temp")
+            NaphthaBase.commit()
+        
+        c.execute("create table Stock_temp (Batch text, Code text, \
+                  Revision text, BatchStatus text, QuantityNow text, \
+                  OriginalDeliveredQuantity text, StockInfo text, \
+                  Supplier text, PONumber text, PurchaseCost text, \
+                  Customer text, WONumber text, Price text, \
+                  UsageReference text, StockAction text, ItemOrder text, \
+                  QuantityMovement text, UserID text, LastUpdated date, \
+                  InvoiceDate date, BatchUp_Date date)")
+
+        for entry in RandRdata:
+            entry_temp = [thing for thing in entry] #prices and quantities are
+            entry_temp[9] = str(entry_temp[9]) #stored as a decimal type in the
+            entry_temp[12] = str(entry_temp[12]) #RandR database. Convert them to text
+            #fields for storing in the NaphthaBase
+            c.execute('insert into Stock_temp values (?,?,?,?,?,?,?,?,?, \
+                      ?,?,?,?,?,?,?,?,?,?,?,?)', entry_temp)
+        NaphthaBase.commit()
+        
+        diffquery = """
+        SELECT Stock_temp.*
+        from Stock_temp
+        Left Join Stock on (Stock_temp.LastUpdated =
+        Stock.LastUpdated)
+        where Stock.LastUpdated IS Null
+        """
+        
+        diff = c.execute(diffquery)
+        newrows =  [row for row in diff]
+        for entry in newrows: 
+            c.execute('insert into Stock values (?,?,?,?,?,?,?,?,?,?,?, \
+                      ?,?,?,?,?,?,?,?,?,?)', entry)
+        NaphthaBase.commit()
+        
+        self.Stockdata = [row for row in c.execute("select * from Stock")]
+        #self._create_db()
+        NaphthaBase.close()
         
 
 if __name__ == '__main__':
