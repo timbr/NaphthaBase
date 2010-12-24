9fd9c22aeea7ee6872026c4530789e83b424f35c
unknown <Tim@.(none)>
2010-12-24 10:41:19 +0000

    Replaced database lookup with list lookup for working out references.

--- populatedb.py
+++ populatedb.py
@@ -16,14 +16,11 @@ def update(data, table):
     nb.naphthabase_transfer(data, 'insert into %s values %s' \
                                 % (table, insert_fields))
 
-def getdata(table):        
-        if table == '':
-        data = [row for row in nb.naphthabase_query("select * from %s" % table)]
-        return data
 
 
 data = getdata(nb.sql.material_codes, 'Material')
 update(data, 'material')
+materialdata = [row for row in nb.naphthabase_query("select code, id from material")]
 
 data = getdata(nb.sql.get_customer, 'Customer')
 newdata = []
@@ -35,7 +32,7 @@ for item in data:
     line[3:] = item[7:]
     newdata.append(line)
 update(newdata, 'customer')
-customerdata =[row for row in nb.naphthabase_query("select customer_code, id from customer")]
+customerdata = [row for row in nb.naphthabase_query("select customer_code, id from customer")]
 
 data = getdata(nb.sql.get_supplier, 'Supplier')
 newdata = []
@@ -55,14 +52,14 @@ newdata = []
 for item in data:
     line = []
     line[0:5] = item[0:5]
-    customercode = nb.naphthabase_query("select id from customer where customer_code='%(code)s'" % {'code': item[0]})
+    customercode = [data[1] for data in customerdata if data[0] == item[0]]
     if len(customercode) > 0:
-        line.append(customercode[0][0])
+        line.append(customercode[0])
     else:
         line.append(None)
-    suppliercode = nb.naphthabase_query("select id from supplier where supplier_code='%(code)s'" % {'code': item[0]})
+    suppliercode = [data[1] for data in supplierdata if data[0] == item[0]]
     if len(suppliercode) > 0:
-        line.append(suppliercode[0][0])
+        line.append(suppliercode[0])
     else:
         line.append(None)
     line[8:] = item[5:]
@@ -74,28 +71,29 @@ newdata = []
 for item in data:
     line = []
     line[0:2] = item[0:2]
-    suppliercode = nb.naphthabase_query("select id from supplier where supplier_code='%(code)s'" % {'code': item[2]})
+    suppliercode = [data[1] for data in supplierdata if data[0] == item[2]]
     if len(suppliercode) > 0:
-        line.append(suppliercode[0][0])
+        line.append(suppliercode[0])
     else:
         line.append(None)
     line[3:] = item[3:]
     newdata.append(line)
 update(newdata, 'purchaseorder')
+purchasedata = [row for row in nb.naphthabase_query("select pon, id from purchaseorder")]
 
 data = getdata(nb.sql.get_purchaseitem, '\"Purchase Item\"')
 newdata = []
 for item in data:
     line = []
     line.append(item[0])
-    purchaseorder = nb.naphthabase_query("select id from purchaseorder where pon='%(pon)s'" % {'pon': item[0]})
+    purchaseorder = [data[1] for data in purchasedata if data[0] == item[0]]
     if len(purchaseorder) > 0:
-        line.append(purchaseorder[0][0])
+        line.append(purchaseorder[0])
     else:
         line.append(None)
-    materialcode = nb.naphthabase_query("select id from material where code='%(code)s'" % {'code': item[1]})
+    materialcode = [data[1] for data in materialdata if data[0] == item[1]]
     if len(materialcode) > 0:
-        line.append(materialcode[0][0])
+        line.append(materialcode[0])
     else:
         line.append(None)
     line[3:] = item[2:]
@@ -108,21 +106,21 @@ newdata = []
 for item in data:
     line = []
     line.append(item[0])
-    materialcode = nb.naphthabase_query("select id from material where code='%(code)s'" % {'code': item[1]})
+    materialcode = [data[1] for data in materialdata if data[0] == item[1]]
     if len(materialcode) > 0:
-        line.append(materialcode[0][0])
+        line.append(materialcode[0])
     else:
         line.append(None)
     line.append(item[2])
     line.append(item[3])
-    suppliercode = nb.naphthabase_query("select id from supplier where supplier_code='%(code)s'" % {'code': item[4]})
+    suppliercode = [data[1] for data in supplierdata if data[0] == item[4]]
     if len(suppliercode) > 0:
-        line.append(suppliercode[0][0])
+        line.append(suppliercode[0])
     else:
         line.append(None)
-    purchaseorder = nb.naphthabase_query("select id from purchaseorder where pon='%(pon)s'" % {'pon': item[5]})
+    purchaseorder = [data[1] for data in purchasedata if data[0] == item[5]]
     if len(purchaseorder) > 0:
-        line.append(purchaseorder[0][0])
+        line.append(purchaseorder[0])
     else:
         line.append(None)
     line[6:] = item[6:]
