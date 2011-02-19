9183081a490d2d18bfc709fca8a82487dd444da8
Tim Browning <tibrowning@gmail.com>
2011-02-19 16:05:47 +0000

    commit before tidying.

--- naphthabase.py
+++ naphthabase.py
@@ -9,12 +9,9 @@ provided.
 """
 
 import pyodbc
-import os
-import sqlite3
 import datetime
-import decimal
-
-from nbsettings import *
+import os
+import nbsettings
 import sql
 import logging
 
@@ -30,9 +27,9 @@ class RandRDatabase(object):
 #////////////////////////////////////////////////////////////////////////////#
 
     def __init__(self):
-        self.naphthav6 = "S:\\NAPHTHAV6\\DATA\\Naphtha.mdb"
-        self.naphtha_accounts = "S:\\NAPHTHAV6\\DATA\\Naphtha_accounts.mdb"
-        self.accounts_db_password = "bgSoiqOogNMOH"
+        self.naphthav6 = nbsettings.RandR_Naphtha_Dbase
+        self.naphtha_accounts = nbsettings.RandR_Accounts_Dbase
+        self.accounts_db_password = nbsettings.accountsDBpassword
         self.connected = 0
         if self.connect() == True:
             self.list_tables()
@@ -104,86 +101,3 @@ class RandRDatabase(object):
         self.disconnect()
         return results
 
-
-#////////////////////////////////////////////////////////////////////////////#
-class NaphthaBase(object):
-    """Creates a connection to the NaphthaBase database.
-    
-    It checks that all necessary tables are present and
-    creates them if necessary. A method is provided for
-    making queries.
-    """
-#////////////////////////////////////////////////////////////////////////////#
-
-    def __init__(self):
-        self.check_tables()
-        
-    def query(sefl, query, params = None):
-        """Returns results of a given NaphthaBase Query.
-    
-        Opens NaphthaBase, runs sql query and closes NaphthaBase.
-        """
-    
-        NaphthaBase = sqlite3.connect(NaphthaBase_Dbase)
-        #NaphthaBase.text_factory = lambda x: unicode(x, "utf-8", "ignore")  # solves problem with Pound signs....!
-        c = NaphthaBase.cursor()
-        if params == None:
-            results = [row for row in c.execute(query)]
-        else:
-            results = [row for row in c.execute(query, params)]
-        NaphthaBase.commit()
-        NaphthaBase.close()
-        return results
-
-    def transfer(self, data, query):
-        """Loads data into NaphthaBase and closes database at the end.
-    
-        Faster than writing each line individually using naphthabase_query()
-        as it keeps the NaphthaBase connection open until all lines have been
-        written.
-        """
-    
-        NaphthaBase = sqlite3.connect(NaphthaBase_Dbase)
-        #NaphthaBase.text_factory = str # solves problem with Pound signs....!
-        c = NaphthaBase.cursor()
-        for entry in data:
-            entry = [None] + entry # Add null value so that primary key autoincrements
-            results = [row for row in c.execute(query, entry)]
-        NaphthaBase.commit()
-        NaphthaBase.close()
-    
-    def check_tables(self):
-        """Checks that the NaphthaBase has all the tables it needs."""
-    
-        # Dictionary of table names and the sql query strings needed to create them
-        tablelist = {'purchaseorder': sql.create_purchaseorder_table,
-                     'purchaseitem': sql.create_purchaseitem_table,
-                     'material': sql.create_material_table,
-                     'stock': sql.create_stock_table,
-                     'stockmovement': sql.create_stockmovement_table,
-                     'salesorder': sql.create_salesorder_table,
-                     'salesitem': sql.create_salesitem_table,
-                     'despatch': sql.create_despatch_table,
-                     'deletedsales': sql.create_deletedsales_table,
-                     'hauliers': sql.create_hauliers_table,
-                     'carrier': sql.create_carrier_table,
-                     'customer': sql.create_customer_table,
-                     'supplier': sql.create_supplier_table,
-                     'contact': sql.create_contact_table,
-                     'depot': sql.create_depot_table}
-        query = \
-          self.query("select * from sqlite_master where type = 'table'")
-        # What tables are in the NaphthaBase?
-        dbtables = [row[1] for row in query]
-        # Excecute sql to create any missing tables
-        for table in tablelist.keys():
-            if table not in dbtables:
-                self.query(tablelist[table])
-
-    def get_columns(self, table):
-        """Creates an ordered list of column names for a given table.
-        """
-    
-        column_data = self.query("pragma table_info(%s)" % table)
-        column_names = [col[1] for col in column_data]
-        return column_names
